6ca554cde027eef900487d6301acf7bb7cfa6411
DSJR1284 <DSJR1284@GMAIL.COM>
2021-02-25 11:23:21 -0500

    Seeded app with data, Ran shout gun to check home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :games
   has_secure_password
-  validates_uniqueness_ of :username
+  validates_uniqueness_of :username
   validates_presence_of :username, :password  
 end
    
