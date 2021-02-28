7e57ec59805ec7aa60b12bbb149fb6d812c131d0
Doug Jones <DSJR1284@GMAIL.COM>
2021-02-28 16:50:57 -0500

    working on edit route and function

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,27 +13,32 @@ class GamesController < ApplicationController
 
   # POST: /games
   post "/games" do
-    game = Game.create(title: params[:title], game_image: params[:game_image], blurb: params[:blurb], user_id: current_user.id)
-    redirect "/games/#{game.id}"
+    games = Game.create(title: params[:title], game_image: params[:game_image], blurb: params[:blurb], user_id: current_user.id)
+    redirect "/games/#{games.id}"
   end
 
   # GET: /games/5
   get "/games/:id" do
+    @games = Game.find_by_id(params[:id])
     erb :"/games/show"
   end
 
   # GET: /games/5/edit
   get "/games/:id/edit" do
+    @games = Game.find_by_id(params[:id])
     erb :"/games/edit"
   end
 
   # PATCH: /games/5
   patch "/games/:id" do
-    redirect "/games/:id"
+    @games = Game.find_by_id(params[:id])
+    @games.update(title: params[:title], game_image: params[:game_image], blurb: params[:blurb])
+    redirect "/games/#{game.id}"
   end
 
   # DELETE: /games/5/delete
   delete "/games/:id/delete" do
     redirect "/games"
   end
+
 end
